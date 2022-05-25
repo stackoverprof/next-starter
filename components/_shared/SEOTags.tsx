3cc8967e634f547c8415294000a29c5e477ccadf
stackoverprof <mail.errbint@gmail.com>
2022-05-25 12:44:46 +0700

    fix: seo tags

--- components/_shared/SEOTags.tsx
+++ components/_shared/SEOTags.tsx
@@ -1,5 +1,3 @@
-// [README] : Pre configured SEO Optimization
-
 import React from 'react';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
@@ -12,39 +10,37 @@ interface SEODataType {
 	image?: string;
 }
 
-const DEFAULT: SEODataType = {
-	sitename: 'Audiobox',
+const DEFAULT = {
+	sitename: 'My Site',
 	domain: 'http://my-site.com/',
-	description: 'Find Tracks and create the Playlist you love.',
+	description: 'Site description that contains some information about what is your site about',
 	image: 'https://images.unsplash.com/photo-1629058622223-93665bf5d046?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
 };
 
 const SEOTags = (props: SEODataType) => {
+	const router = useRouter();
+
 	const data = { ...DEFAULT, ...props };
-	const { pathname } = useRouter();
-	const url = window.location.origin + pathname;
 	const supertitle = data.title ? `${data.title} | ${data.sitename} ` : data.sitename;
 
 	return (
 		<Head>
-			<meta charSet="utf-8" />
-
 			<title>{supertitle}</title>
 
 			<meta name="robots" content="follow, index" />
 			<meta name="description" content={data.description} />
-			<meta property="og:url" content={url} />
-			<link rel="canonical" href={url} />
+			<meta property="og:url" content={router.asPath} />
+			<link rel="canonical" href={router.asPath} />
 
 			<meta property="og:type" content="website" />
-			<meta property="og:title" content={data.title} />
-			<meta property="og:site_name" content={data.title} />
+			<meta property="og:title" content={supertitle} />
+			<meta property="og:site_name" content={supertitle} />
 			<meta property="og:description" content={data.description} />
 			<meta property="og:image" name="image" content={data.image} />
 
 			<meta name="twitter:card" content="summary_large_image" />
-			<meta name="twitter:site" content={data.title} />
-			<meta name="twitter:title" content={data.title} />
+			<meta name="twitter:site" content={supertitle} />
+			<meta name="twitter:title" content={supertitle} />
 			<meta name="twitter:description" content={data.description} />
 			<meta name="twitter:image" content={data.image} />
 			<meta
